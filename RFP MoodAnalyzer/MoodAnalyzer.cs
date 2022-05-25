e3afc281ff3c548316c8965022f3b6181304ddaf
Komal <divyavairagade@gmail.com>
2022-05-25 14:02:24 +0530

    To analyse the mood is happy or sad

---
+++ RFP MoodAnalyzer/MoodAnalyzer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RFP_MoodAnalyzer
+{
+    public class MoodAnalyzer
+    {
+        string message;
+        public MoodAnalyzer(string message)
+        {
+            this.message = message;
+        }
+        public string AnalyseMethod()
+        {
+            if (message.ToLower().Contains("sad"))
+            {
+                return "sad";
+            }
+            return "happy";
+        }
+    }
+}
---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,26 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RFP_MoodAnalyzer;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class TestMoodAnalysis
+    {
+        [TestMethod]
+        [DataRow("I am in a sad Mood ")]
+        public void GivenStringInput_WhenTestMoodAnalysis_shouldReturnSad(string message)
+        {
+            MoodAnalyzer moodAnalysis = new MoodAnalyzer(message);
+            string mood = moodAnalysis.AnalyseMethod();
+            Assert.AreEqual(mood, "sad");
+        }
+        [TestMethod]
+        [DataRow("I am in a happy Mood")]
+        public void GivenStringInput_WhenTestMoodAnalysis_shouldReturnHappy(string message)
+        {
+            MoodAnalyzer moodAnalysis = new MoodAnalyzer(message);
+            string mood = moodAnalysis.AnalyseMethod();
+            Assert.AreEqual(mood, "happy");
+        }
+    }
+}
